598add499d2aa2ca6538f443930b6f6d27bfc526
Vaishnavi428 <110715513+Vaishnavi428@users.noreply.github.com>
2023-01-06 17:03:39 +0530

    Create Home.js

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+function Home() {
+    return (
+      <div className="m-5 p-5">
+       
+      <h4>Hello, </h4>
+      <h1>I am User Name</h1>
+      <h5 className="text-danger my-2">Full Stack Developer</h5>
+      <div className="btn btn-danger">See my profile</div>
+  
+      </div>
+    );
+  }
+  
+  export default Home;
